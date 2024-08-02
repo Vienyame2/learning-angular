5d0afc4d761eae5ac516c3bfdb3609b1396a3b8e
Achref ABDENNEBI <achref.abdennebi.ext@suez.com>
2024-08-02 16:08:48 +0200

    feat: init styles

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+  ],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent {
-
+    public title = new FormControl('');
 }
