9eb5c37b3a47719c38567f03dd1ada5d8a53ad73
Achref ABDENNEBI <achref.abdennebi.ext@suez.com>
2024-09-06 16:23:55 +0200

    feat: add edit mode to todos

--- src/app/todos/services/todo-list.service.ts
+++ src/app/todos/services/todo-list.service.ts
@@ -14,6 +14,20 @@ export class TodoListService {
   public todoCount = computed(() => this.state().count);
 
   public add(item: TodoItem) {
+    item.id = crypto.randomUUID();
     this.state.update(state => ({ ...state, items: [...state.items, item], count: state.count + 1 }));
+  }
+
+  public update(todoItem: TodoItem) {
+    const updatedList = this.state().items.map(item => {
+      if(item.id === todoItem.id) {
+        return todoItem;
+      }
+      return item;
+    });
+
+    this.state.update((state) => ({
+        ...state, items: [...updatedList]
+    }))
   }
 }
