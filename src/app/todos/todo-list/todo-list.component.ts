d5956e963137f3ec8198120653a0b470c9ed7a76
Achref ABDENNEBI <achref.abdennebi.ext@suez.com>
2024-10-04 16:37:15 +0200

    feat: add counters state

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, inject, Signal, ViewChild } from '@angular/core';
+import { Component, computed, inject, Signal, ViewChild } from '@angular/core';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
 import { TodoListService } from '../services/todo-list.service';
 import { TodoItem } from '../models/todo-item.model';
 import { MatDivider } from '@angular/material/divider';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
     selector: 'app-todo-list',
     standalone: true,
-    imports: [TodoItemComponent, ReactiveFormsModule, MatFormField, MatDivider],
+    imports: [TodoItemComponent, ReactiveFormsModule, MatFormField, MatDivider, MatBadge],
     templateUrl: './todo-list.component.html',
     styleUrl: './todo-list.component.scss',
 })
@@ -21,6 +22,11 @@ export class TodoListComponent {
     public todoList: Signal<TodoItem[]> = this.todoService.todoList;
     public completedTodos: Signal<TodoItem[]> = this.todoService.completedTodos;
     public todoCount: Signal<number> = this.todoService.todoCount;
+    public todoCounter: Signal<{ active: number; completed: number; total: number }> = computed(() => ({
+        total: this.todoCount(),
+        active: this.todoCount() - this.completedTodos().length,
+        completed: this.completedTodos().length,
+    }));
 
     constructor() {}
 
